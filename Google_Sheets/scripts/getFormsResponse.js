9c094d32599ff6e9bf5b7c9f989f4444b01a2ab6
BOUAKKAZ Djallel Eddine <bouakaz.djallel@gmail.com>
2024-06-18 06:43:41 +0200

    📦 NEW: Notification des admins

--- Google_Sheets/scripts/getFormsResponse.js
+++ Google_Sheets/scripts/getFormsResponse.js
@@ -4,16 +4,78 @@ function getFormsResponse(e) {
   console.log("Un formulaire a été soumis");
 
   const formResponses = e.namedValues;
-  const tirelireData = getSheetDataByName(SHEET_DEF.TIRELIRE.SHEET_NAME);
   const tirelireSheet = getSheetByName(SHEET_DEF.TIRELIRE.SHEET_NAME);
+  const tirelireData = getSheetDataByName(SHEET_DEF.TIRELIRE.SHEET_NAME);
   const questions = SHEET_DEF.QUESTIONS_FORM;
-  let montant = "";
+
   const id_event = getFormQuestion(formResponses, questions.ID_EVENT);
   const note = getFormQuestion(formResponses, questions.NOTE_RECUP);
   const commentaire = getFormQuestion(formResponses, questions.COMM_RECUP);
+  const currResponsableMail = getFormQuestion(formResponses, questions.EMAIL);
+
+  let montant = "";
 
   if (getFormQuestion(formResponses, questions.TRANSFERER) === "Non") {
-    // TODO: Envoyer mail aux admins
+    console.warn("Le frère est dans l'incapacité de récupérer sa tirelire");
+
+    const frereData = getSheetDataByName(SHEET_DEF.FRERE.SHEET_NAME);
+    console.log("Récupération de la liste des administrateurs à notifier");
+
+    const listAdmin = frereData
+      .filter(
+        (row) =>
+          row[getColumnIndex("FRERE", "ROLE")].toLowerCase() ===
+          "administrateur"
+      )
+      .map((row) => row[getColumnIndex("FRERE", "MAIL")])
+      .join(",");
+
+    console.log("Récupération des information de la tirelire");
+    const targetRow = tirelireData.find((row) => {
+      if (row[getColumnIndex("TIRELIRE", "DATE_RETRAIT")] === "") {
+        return row[getColumnIndex("TIRELIRE", "ID_EVENT")] === id_event;
+      }
+      return false;
+    });
+
+    if (!targetRow) {
+      console.error(`Aucun événement trouvé avec l'ID ${id_event}`);
+      return;
+    }
+
+    console.log(
+      `L'événement ${id_event} a été trouvé. Traitement de la réponse...`
+    );
+    const rowIndex = tirelireData.indexOf(targetRow) + 2;
+    const currMagasin = tirelireSheet
+      .getRange(rowIndex, getRealColumnIndex("TIRELIRE", "MAGASIN"))
+      .getValue();
+    const currentUser = getUserDetailsByMail(currResponsableMail);
+
+    if (!currentUser) {
+      console.error(
+        "Utilisateur actuel non trouvé pour l'e-mail : " + currResponsableMail
+      );
+      return;
+    }
+
+    const subject = "Tirelire du magasin " + currMagasin;
+    const body =
+      "Le frère " +
+      currentUser.nom +
+      " " +
+      currentUser.prenom +
+      " est dans l'incapacité de récupérer sa tirelire pour les raisons suivantes : \n\n" +
+      commentaire;
+    const options = {
+      name: "AMANA",
+      noReply: true,
+    };
+    try {
+      MailApp.sendEmail(listAdmin, subject, body, options);
+    } catch (error) {
+      console.error("Échec de l'envoi du mail: " + error.toString());
+    }
     return;
   }
 
@@ -93,11 +155,7 @@ function getFormsResponse(e) {
         `Creation de l'événement sur le calendrier à la date du ${newDeadline}`
       );
 
-      findAndReScheduleEvent(
-        calendar,
-        id_event,
-        newDeadline
-      );
+      findAndReScheduleEvent(calendar, id_event, newDeadline);
     } else if (cdtTransferer) {
       const newFrere = getFormQuestion(
         formResponses,
@@ -105,12 +163,7 @@ function getFormsResponse(e) {
       );
       console.log(`Le frère souhaite transférer la tirelire à ${newFrere}`);
 
-      transferResponsable(
-        calendar,
-        id_event,
-        newFrere,
-        newDeadline
-      );
+      transferResponsable(calendar, id_event, newFrere, newDeadline);
     }
   }
 }
